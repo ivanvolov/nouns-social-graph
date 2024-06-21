0cd09af0ac2956f931e51cf9cd421acc902adb78
ivan <vivan.volovik@gmail.com>
2024-06-21 21:23:56 +0200

    data mining preprocessing

---
+++ frontend/src/WalletConnect.js
@@ -0,0 +1,9 @@
+import { WalletConnectModalSign, useConnect, useRequest } from "@walletconnect/modal-sign-react";
+
+const projectId = "dkdj83003jkd";
+
+const WalletConnect = () => {
+    return <>dddddd</>;
+};
+
+export default WalletConnect;
