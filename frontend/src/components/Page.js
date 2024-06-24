8f1d2a1874b64a3c7b04eabb55024e2f68cb28a8
ivan <vivan.volovik@gmail.com>
2024-06-24 11:44:47 +0200

    removing remote and old frontentd stuff

--- frontend/src/components/Page.js
+++ frontend/src/components/Page.js
@@ -1,11 +1,6 @@
 import React from "react";
 import Card from "./Card";
-import data1 from "../data/trending_21_06_24.json";
-import discoveries1 from "../data/small_20_06_24.json";
-import discoveries2 from "../data/small_18_06_24.json";
-import data2 from "../data/trending_19_06_24.json";
-import topData from "../data/top.json";
-import Minter from "./child/Minter";
+import community from "../data/trending_21_06_24.json";
 import ConnectButton from "./ConnectButton";
 
 const Page = () => {
@@ -54,43 +49,13 @@ const Page = () => {
                 <div class="container">
                     <div class="col-9">
                         <Card
-                            initialData={data1}
+                            initialData={community}
                             date="2 days ago"
-                            node_cont={data1.nodes.length}
-                            title={"Zora | Trending"}
-                        ></Card>
-                        <Card
-                            initialData={discoveries1}
-                            date="3 days ago"
-                            node_cont={discoveries1.nodes.length}
-                            title={"Zora | discoveries"}
-                        ></Card>
-                        <Card
-                            initialData={data2}
-                            date="5 days ago"
-                            node_cont={data2.nodes.length}
-                            title={"Zora | Trending"}
-                        ></Card>
-                        <Card
-                            initialData={discoveries2}
-                            date="6 days ago"
-                            node_cont={discoveries1.nodes.length}
-                            title={"Zora | discoveries"}
-                        ></Card>
+                            node_cont={community.nodes.length}
+                            title={"Base | Communitys"}
+                        >
                     </div>
                     <div class="col-3">
-                        <div class="card">
-                            <h4>Top minters</h4>
-                            {topData.minters.map((minter) => (
-                                <Minter name={minter.id} image={minter.image} minter="true"></Minter>
-                            ))}
-                        </div>
-                        <div class="card">
-                            <h4>Top projects</h4>
-                            {topData.projects.map((minter) => (
-                                <Minter name={minter.id} image={minter.image}></Minter>
-                            ))}
-                        </div>
                     </div>
                 </div>
             </div>
